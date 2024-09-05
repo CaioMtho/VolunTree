d1be3d7359972eac208601fe96df03469d4d8969
HUDSON SANTOS <hudsonhenrique013@gmail.com>
2024-09-05 20:55:22 -0300

    .

---
+++ VolunTree_API/Models/Voluntario_ongs.cs
@@ -0,0 +1,11 @@
+﻿namespace VolunTree_API.Models
+{
+    public class Voluntario_ongs
+    {
+		public int IdLigacao { get; set; }
+		public string Cpf { get; set; }
+		public string Cnpj { get; set; }
+		public DateTime DataInteresse { get; set; }
+
+	}
+}
