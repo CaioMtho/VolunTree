9ae91e277a8ce808c023e1263967ced28d8ff89b
HUDSON SANTOS <hudsonhenrique013@gmail.com>
2024-09-05 21:01:47 -0300

    Criação do controle Sugestoes

---
+++ VolunTree_API/Models/Sugestoes.cs
@@ -0,0 +1,12 @@
+﻿namespace VolunTree_API.Models
+{
+    public class Sugestoes
+    {
+		public int IdSuges { get; set; }
+		public string Cpf { get; set; }
+		public string Cnpj { get; set; }
+		public string Score { get; set; }
+		public DateTime DataSugestao { get; set; }
+
+	}
+}
