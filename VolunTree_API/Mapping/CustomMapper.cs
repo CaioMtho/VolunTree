96f454d63111cdc8c187f1ed2d0df9015477af78
Caio Matheus de Oliveira <caiomathol@gmail.com>
2024-09-13 00:04:05 -0300

    Configurar mapeamento do Dapper

---
+++ VolunTree_API/Mapping/CustomMapper.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace SeuProjeto.Mappings
+{
+    public class CustomMapper : SqlMapper.ITypeMap
+    {
+        private readonly SqlMapper.ITypeMap _default;
+
+        public CustomMapper(Type type)
+        {
+            _default = new CustomPropertyTypeMap(
+                type,
+                (t, columnName) => t.GetProperties().FirstOrDefault(prop => prop.Name.Equals(
+                    string.Join("", columnName.Split('_').Select(word => char.ToUpper(word[0]) + word.Substring(1))),
+                    StringComparison.OrdinalIgnoreCase))
+            );
+        }
+
+        public ConstructorInfo FindConstructor(string[] names, Type[] types)
+        {
+            return _default.FindConstructor(names, types);
+        }
+
+        public ConstructorInfo FindExplicitConstructor()
+        {
+            return _default.FindExplicitConstructor();
+        }
+
+        public SqlMapper.IMemberMap GetConstructorParameter(ConstructorInfo constructor, string columnName)
+        {
+            return _default.GetConstructorParameter(constructor, columnName);
+        }
+
+        public SqlMapper.IMemberMap GetMember(string columnName)
+        {
+            return _default.GetMember(columnName);
+        }
+    }
+}
--- VolunTree_API/Program.cs
+++ VolunTree_API/Program.cs
@@ -1,9 +1,12 @@
+using Dapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using VolunTree_API.Services;
 using System.Text.Json;
+using SeuProjeto.Mappings;
+using VolunTree_API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +29,11 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
 });
+
+SqlMapper.SetTypeMap(typeof(Voluntario), new CustomMapper(typeof(Voluntario)));
+SqlMapper.SetTypeMap(typeof(Ong), new CustomMapper(typeof(Ong)));
+SqlMapper.SetTypeMap(typeof(VoluntarioOng), new CustomMapper(typeof(VoluntarioOng)));
+SqlMapper.SetTypeMap(typeof(Sugestao), new CustomMapper(typeof(Sugestao)));
 
 var app = builder.Build();
 
